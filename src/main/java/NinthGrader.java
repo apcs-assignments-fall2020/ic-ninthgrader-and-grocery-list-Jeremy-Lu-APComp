f6190b2c76e6ad403a293154c982cd84d111a60e
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-11-02 16:42:23 +0000

    Added code for NinthGrader class

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,17 @@
-public class NinthGrader {
-    
+public class NinthGrader extends Student {
+    public NinthGrader(String a, String b) {
+        super(a, b, 2024);
+    }
+
+    // A default/no-argument constructor
+    public NinthGrader() {
+        super("Bob", "Dylan", 2024);
+    }
+    public void doSomething(){
+        System.out.println("I am preparing for a Harkness discussion!");
+    }
+    public String toString(){
+        String str = this.getFirstName() + " " + this.getLastName() + " " + this.getClassYear() + " (Ninth Grader)";
+        return str;
+    }
 }
--- src/main/java/NinthGraderClient.java
+++ src/main/java/NinthGraderClient.java
@@ -1,3 +1,6 @@
 public class NinthGraderClient {
-    
+    public static void main(String[] args) {
+        NinthGrader n1 = new NinthGrader("Steve", "Stevenson", 2024);
+        System.out.println(n1);
+    }
 }
